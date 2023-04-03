5a0dfe8c79be3ed7d76b964592c29c0a8e1f203f
Sascha <Tohwa.gaming@gmail.com>
2023-04-03 11:03:11 +0200

    added work folder

---
+++ EscapeRoom/EscapeRoom/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace EscapeRoom
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsoleDesign.SetWindowSize();
+            Console.WriteLine("Hello World");
+        }
+    }
+}
